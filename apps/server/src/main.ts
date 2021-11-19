745f6b6f22b68ea4d4ee63bc39e024ebb312a786
baijanathTharu <bnchaudhary.tharu@gmail.com>
2021-11-19 15:54:50 +0545

    initial commit

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,16 +2,46 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
+import * as dotenv from 'dotenv';
 import * as express from 'express';
+import * as cors from 'cors';
+import { User } from '@express-next-starter/shared-types';
 
+dotenv.config();
 const app = express();
+
+console.log('test', process.env.PORT);
+
+app.use(
+  cors({
+    origin: 'http://localhost:4200',
+  })
+);
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to server!' });
 });
 
-const port = process.env.port || 3333;
+app.get('/posts', async (req, res) => {
+  const data: User[] = [
+    {
+      userId: 1,
+      id: 1,
+      title:
+        'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'qui est esse',
+      body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+    },
+  ];
+  res.send(data);
+});
+
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,34 @@
+import styles from './index.module.scss';
+import React from 'react';
+import { User } from '@express-next-starter/shared-types';
+
+export function Index() {
+  const [posts, setPosts] = React.useState<User[]>([]);
+
+  console.log('test', process.env.NEXT_PUBLIC_TEST);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3333/posts', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      {posts?.map((p) => {
+        return (
+          <div key={p.id}>
+            <h3>{p.title}</h3>
+            <p>{p.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Index;
